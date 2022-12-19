35c8799a6416bef009c55059c3a0dc47eb8519b4
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2022-12-19 22:51:06 +0300

    homepage ui update

--- app/src/main/java/com/example/coinmarket/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/coinmarket/ui/detail/DetailFragment.kt
@@ -1,16 +1,10 @@
 package com.example.coinmarket.ui.detail
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
-import com.example.coinmarket.R
 import com.example.coinmarket.base.BaseFragment
 import com.example.coinmarket.databinding.FragmentDetailBinding
 import com.example.coinmarket.model.detail.CoinDetail
@@ -21,6 +15,7 @@ import com.google.gson.Gson
 import dagger.hilt.android.AndroidEntryPoint
 import org.json.JSONArray
 import org.json.JSONObject
+
 
 @AndroidEntryPoint
 class DetailFragment : BaseFragment<FragmentDetailBinding, DetailViewModel>(
@@ -57,7 +52,7 @@ class DetailFragment : BaseFragment<FragmentDetailBinding, DetailViewModel>(
         val jsonObject = JSONObject(json)
         val jsonArray = jsonObject[args.symbol] as JSONArray
 
-        val coin = gson.fromJson(jsonArray.getJSONArray(0).toString(), CoinDetail::class.java)
+        val coin = gson.fromJson(jsonArray.getJSONObject(0).toString(), CoinDetail::class.java)
 
         coin?.let {
             with(binding){
--- app/src/main/java/com/example/coinmarket/utils/Constants.kt
+++ app/src/main/java/com/example/coinmarket/utils/Constants.kt
@@ -3,5 +3,5 @@ package com.example.coinmarket.utils
 object Constants {
     const val BASE_URL = "https://pro-api.coinmarketcap.com/"
     const val API_KEY = "8c62a913-a3c0-4952-a8e8-6f9330ce0605"
-    const val LIMIT = "20"
+    const val LIMIT = "50"
 }
